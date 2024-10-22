30fd4b3510aa5628e61953f047b034a77fe01663
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-10-22 10:27:43 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,4 @@
 var who66 ='world';
 function sayhello66(){alert('Hello,'+ who66 + '!');}
-function taro66(){ who66 ='太郎が来た';}
-function hanako66(){ who66 = '花子が来た';}
+function tarogakita66(){ who66 ='太郎が来た';}
+function hanakogakita66(){ who66 = '花子が来た';}
