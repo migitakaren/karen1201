dd4b12429dc9291503f3e9446c2898966c131731
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-11-25 22:35:44 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){
-    document.getElementByld('logo').src='./image/bulbon.gif';}
+function imgchange1(){document.getElementById('mark').src='./image/sumaho2.png';}
+function imgchange2(){document.getElementById('mark').src='./image/sumaho.png';}
