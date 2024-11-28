311d9484d25fddc42de7c87443b1ad4bfbc1d7f0
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-11-28 21:19:47 +0900

    Create ex6-11.js

---
+++ ex6-11.js
@@ -0,0 +1,6 @@
+function imgchange3() {
+    document.getElementById('logo').src = './image/jousaikyara.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josailogo.jpg';
+}
