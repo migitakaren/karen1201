2ef2aaaff4ece6ca603e584e71dfe09fafa8abfd
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-12-10 10:24:00 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('neko1').src'./image/R.jpg';}
-function mes2(){document.getElementById('neko1').src'./image/neco2.jpg';}
+function imgchange5(){document.getElementByld('neko1').src'./image/R.jpg';}
+function mes2(){document.getElementByld('neko1').src'./image/neco2.jpg';}
