5374feeb39d84bbb63b67cdd69e136c16808ff81
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-12-21 22:14:37 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,3 +1,4 @@
-dfunction add73(){var a=document.getElementById('input731').value;
-                  var b=document.getElementById('input732').value;
-                  alert(Nunber(a)/Number(b)*Number(b)));}
+function add73(){
+  var a=document.getElementById('input731').value;
+  var b=document.getElementById('input732').value;
+  alert(Number(a)/(Number(b)*Number(b)));}
